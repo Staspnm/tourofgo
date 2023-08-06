4e1f6bafa027b0dd66d679a12a26d2294c364575
PnmStas <pnmstas@yandex.ru>
2023-08-06 19:15:58 +0500

    YY/MM/DD
     1 lesson

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 
-	fmt.Println("Hi go")
+	fmt.Println("Welcome to the playground!")
 
+	fmt.Println("The time is", time.Now())
 }
